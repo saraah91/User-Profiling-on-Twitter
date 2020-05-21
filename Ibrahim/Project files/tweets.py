35cd0d9c9f3b215f503a74675777295bb18b919d
ibrahim-ih1518019 <ih1518019@qu.edu.qa>
2020-05-21 10:34:57 +0300

    combined the files together

--- Ibrahim/Project files/tweets.py
+++ Ibrahim/Project files/tweets.py
@@ -1,18 +1,16 @@
-'''
 import tweepy
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler('SMMEliDrPSMnCX9JkX1jDdjK5', 'JyTaaIfHgBVkOzCiutPjhh5HKAv3bCt4M4Y3dn8W8iTiDYS4cX')
+auth.set_access_token('856672885-NggrT8H1xP1XA8eiV8WEkAwXNbLBAcCqUkevZu9X', 'ZWLYD0WJKjulXWX1QcMIAYSpT3qr8C0Jatim3FNy9LOOm')
 
 api = tweepy.API(auth)
-
+'''
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
-
-user = api.get_user('twitter')
+'''
+user = api.get_user('Cookie_Brho')
 print(user.screen_name)
 print(user.followers_count)
 for friend in user.friends():
    print(friend.screen_name)
-'''
