ed5ca9bff45b1597a1f11b47d2abca651036bb79
saraah91 <43216921+saraah91@users.noreply.github.com>
2020-05-21 13:25:05 +0300

    Add files via upload

--- sarah/javascript.js
+++ sarah/javascript.js
@@ -1,3 +1,24 @@
 function showDiv() {
    document.getElementById('stats').style.display = "block";
+   //add something to update html based on json file
+}
+function sendFile(){
+    var reader = new FileReader();
+    reader.onload = onReaderLoad;
+    reader.readAsText(document.querySelector('input').files[0]);
+
+
+    function onReaderLoad(event){
+      var data = JSON.stringify(event.target.result);
+      console.log(data);
+    }
+
+
+$.post('/analyze', {
+        data: "data",
+    }).done(function(response) {
+       numTweets = response["numTweets"]
+    // display numTweets on the DOM
+    })
+
 }
